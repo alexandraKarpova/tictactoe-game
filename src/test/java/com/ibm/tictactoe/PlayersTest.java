ed6b6399bde79d767478dffefc7725345b377765
Karpava_A <aliaksandra.karpava@sk.ibm.com>
2017-10-04 14:50:56 +0300

    players creation with all green tests and refactoring

--- src/test/java/com/ibm/tictactoe/PlayersTest.java
+++ src/test/java/com/ibm/tictactoe/PlayersTest.java
@@ -2,25 +2,30 @@ package com.ibm.tictactoe;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 
+@DisplayName("Players Test")
 public class PlayersTest {
 
     private TicTacToeGame ticTacToeGame = new TicTacToeGame();
 
+    @DisplayName("Player X created")
     @Test
     public void PlayerXCreated() {
         ticTacToeGame.createPlayerX();
         Assertions.assertEquals("x", ticTacToeGame.getPlayerX().getMark());
     }
 
+    @DisplayName("Player O created")
     @Test
     public void PlayerOCreated() {
         ticTacToeGame.createPlayerO();
         Assertions.assertEquals("o", ticTacToeGame.getPlayerO().getMark());
     }
 
+    @DisplayName("Players created")
     @Test
-    public void NoExtraPlayersCreated() {
+    public void PlayersCreated() {
         ticTacToeGame.createPlayerX();
         ticTacToeGame.createPlayerO();
         Assertions.assertAll(() -> Assertions.assertNotNull(ticTacToeGame.getPlayerX())
